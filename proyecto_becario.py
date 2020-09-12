20f4b971336a8c90cd651de73c726d3bd30c59a1
EdgarFrancoA01707602 <70041765+EdgarFrancoA01707602@users.noreply.github.com>
2020-09-11 19:48:43 -0500

    Update and rename AvanceProyecto2.py to proyecto_becario.py
    En este tercer avance lo que hice fue ya hacer los cambios sobre un mismo archivo como se me sugería en la retroalimentación.
    Para este avance lo que hice fue quitar la pregunta de si aprobaste todas tus materias y meterlo dentro de un if que fuera verificando que todas tuvieran una calificación arriba o igual a 70 y en caso de que no fuera así el contador aprob va a contar la cantidad de materias reprobadas y te la mostraría al final.
    Ya desde antes se habían agregado condicionales por lo que no era necesario agregar mas por el momento y en este caso el aprob participa en dos, uno que sirve para contar las materias reprobadas y en el otro para decirte si aprobaste todas o cuantas habías reprobado. También afecta en la condición de si conservas tu beca ya que uno de los requisitos es tener todas tus materia aprobadas.

--- proyecto_becario.py
+++ proyecto_becario.py
@@ -8,7 +8,7 @@ def function_prestamo(pagos, estat):
         print("Se te retirara tu préstamo educativo")
 
 def function_beca(suma, uni, carg, beca, aprob):
-    if((function_promedio(suma,uni))>=85 and carg>=18 and beca==70 and aprob=="si"):
+    if((function_promedio(suma,uni))>=85 and carg>=18 and beca==70 and aprob==0):
         print("Conservas tu beca")
     else:
         print("Se te retirara la beca")
@@ -16,22 +16,27 @@ def function_beca(suma, uni, carg, beca, aprob):
 uni=int(input("¿Cuántas unidades llevaste? "))
 cont=1
 suma=0
+aprob=0
 
 while (cont<=uni):
     cali=float(input("Calificación de la unidad {} sobre 100: ".format(cont)))
+    if (cali<70):
+        aprob=aprob+1
     suma=suma+cali
     cont=cont+1
 
 carg=int(input("¿Cuánta carga académica llevaste (créditos)? "))
 beca=int(input("¿Cuántas horas de servicio de becario hiciste? "))
-aprob=str(input("Escribe 'si' si aprobaste tus unidadades o 'no' si no aprobaste todas tus unidades "))
 estat=str(input("¿Tu estatus academico es regular? Escribe 'si' o 'no' "))
 pagos=str(input("¿Estas al corriente de tus pagos? Escribe 'si' o 'no' "))
 
 print("\nTu promedio general es: ", function_promedio(suma,uni))
 
+if(aprob>=1):
+    print("Reprobaste ",aprob," materias")
+else:
+    print("Aprobaste todas tus materias")
+
 function_prestamo(pagos, estat)
 
 function_beca(suma, uni, carg, beca, aprob)
-
-

2cf3b507551893cc50d386fdbef53a0f14f53835
EdgarFrancoA01707602 <70041765+EdgarFrancoA01707602@users.noreply.github.com>
2020-09-24 20:22:54 -0500

    Implementación de la librería, nuevo calculo de promedio y mas cálculos dentro del while
    Se hicieron cambios de algunas variables y se agregaron unas nuevas para que se quitaran las preguntas de la cantidad de créditos que llevaste y si tienes un estatus regular, y agregar la pregunta de la cantidad de créditos que equivale cada unidad.
    
    En este avance lo que se hace es pedir la cantidad de créditos que equivale cada unidad dentro el while, que es el elemento de ciclos que se debe de ver el avance numero 4 pero en mi caso ya lo había implementado desde los primeros avances para pedir una cantidad de calificaciones dependiendo de las unidades que haya llevado en el semestre. Lo que se hace dentro del while es pedir la calificación de la unidad y la cantidad de créditos que equivale esa unidad para que estos dos datos sean guardados en contadores hasta que estén registrados los datos de todas las unidades cursadas.
    
    También se hizo la implementación de PYTHON IMAGE LIBRARY que es una librería que nos permite manejar y procesar imágenes, pero en este caso se hace un uso básico ya que en la parte final se pregunta si quieres saber como se calcula el promedio y en caso de responder de forma afirmativa te va a mostrar una imagen en donde se explica como se calcula el promedio actualmente y puede resultar confuso si no sabes este dato.
    
    Primero lo que se debe de hacer es instalar la librería pillow, en mi caso como hago uso de Thonny para instalarlo lo que hice es ir a la pestaña "Tools" y seleccionar "Manage Packages...", ahí lo que se hace es buscar el nombre de la librería y dar clic en "Install".
    Después al inicio del código la tenemos que importar de la siguiente manera "from PIL import Image" ya que aqui estamos diciendo que estamos importando de "PIL" la clase o función "Image".
    Lo que se hace después es buscar la imagen y ponerla en el mismo directorio del archivo para que se pueda abrir, esta parte ya va a ir dentro del if en donde comprobamos si la repuesta a la pregunta "¿Quieres ver como se calcula el promedio?" es "si" para que primero abramos lo que es la imagen y luego mostrarla y en caso de que no se encuentre la imagen mostrar el mensaje "No se pudo cargar la imagen", ya si la respuesta a la pregunta fue "no" solo se terminaría el programa.
    Esto se hizo para demostrar la competencia "Tecnologías SEG0702A" en donde tenemos que usar una biblioteca y explicar su función.

--- proyecto_becario.py
+++ proyecto_becario.py
@@ -1,8 +1,10 @@
-def function_promedio(suma,uni):
-    return suma/uni
+from PIL import Image
 
-def function_prestamo(pagos, estat):
-    if(pagos=="si" and estat=="si"):
+def function_promedio(suma,uni):
+    return suma/carg
+
+def function_prestamo(pagos, credi_r):
+    if(pagos=="si" and credi_r<9):
         print("Conservas tu préstamo educativo")
     else:
         print("Se te retirara tu préstamo educativo")
@@ -12,31 +14,53 @@ def function_beca(suma, uni, carg, beca, aprob):
         print("Conservas tu beca")
     else:
         print("Se te retirara la beca")
+        
+def function_creditos(credi_r):
+    if (credi_r>=9):
+        print("Tu estatus academico es Condicional")
+    else:
+        print("Tu estatus academico es Regular")
 
-uni=int(input("¿Cuántas unidades llevaste? "))
 cont=1
 suma=0
 aprob=0
+carg=0
+credi_r=0
+
+uni=int(input("¿Cuántas unidades llevaste al semestre? "))
 
 while (cont<=uni):
     cali=float(input("Calificación de la unidad {} sobre 100: ".format(cont)))
+    credi=int(input("Cantidad de creditos que equivale la unidad {}: ".format(cont)))
     if (cali<70):
         aprob=aprob+1
+        credi_r=credi_r+credi
+    cali=credi*cali
+    carg=carg+credi
     suma=suma+cali
     cont=cont+1
 
-carg=int(input("¿Cuánta carga académica llevaste (créditos)? "))
 beca=int(input("¿Cuántas horas de servicio de becario hiciste? "))
-estat=str(input("¿Tu estatus academico es regular? Escribe 'si' o 'no' "))
 pagos=str(input("¿Estas al corriente de tus pagos? Escribe 'si' o 'no' "))
 
-print("\nTu promedio general es: ", function_promedio(suma,uni))
+print("\nTu promedio general es: ", "%.2f"% function_promedio(suma,uni))
 
 if(aprob>=1):
     print("Reprobaste ",aprob," materias")
 else:
     print("Aprobaste todas tus materias")
 
-function_prestamo(pagos, estat)
+function_prestamo(pagos, credi_r)
+
+function_creditos(credi_r)
 
 function_beca(suma, uni, carg, beca, aprob)
+
+mostrar=str(input("¿Quieres ver como se calcula el promedio? "))
+
+if(mostrar=="si"):
+    try:
+        img = Image.open("promedio.jpg")
+        img.show()
+    except:
+        print("No se pudo cargar la imagen")

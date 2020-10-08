0cb4e40ec6dcd6e617e8e142ff3d29d6840f1c8f
EdgarFrancoA01707602 <70041765+EdgarFrancoA01707602@users.noreply.github.com>
2020-10-08 18:28:46 -0500

    Avance proyecto 6
    Lo que se pedía en este avance es hacer uso de lo que son las listas anidadas o matrices, así que lo que hice fue que en lugar de usar una lista para guardar las calificaciones y otra para guardar los créditos que vale cada unidad, ya solo use una lista anidada para que guardara ambos datos. Gracias a esto el mayor cambio que hice fue en las funciones ya que en lugar de pedir dos listas ya solo pedía la lista anidada de nombre "unidades" y con esa lista ya era mas fácil sacar datos como el promedio, créditos reprobados, unidades reprobadas y la carga académica. También lo que hice fue que los datos anteriores estuvieran dentro de funciones distintas para un mejor orden. Por ultimo, para aprovechar mejor la lista anidada, lo que hice fue agregar una función que imprima un tipo de tabla con los datos de las unidades ingresadas como lo es el numero de la unidad, calificación, créditos que vale y su ponderación.

--- proyecto_becario.py
+++ proyecto_becario.py
@@ -1,71 +1,108 @@
 from PIL import Image
 
-def function_promedio(cali_lista, credi_lista):
+def function_promedio(unidades):
     acum_cali=0.0
     acum_credi=0
     pos=0
-    for elem in cali_lista:
-        acum_cali=acum_cali+((cali_lista[pos])*(credi_lista[pos]))
-        acum_credi=acum_credi+credi_lista[pos]
+    for cali in unidades:
+        acum_cali=acum_cali+(unidades[pos][0]*unidades[pos][1])
+        acum_credi=acum_credi+unidades[pos][1]
         pos=pos+1
     return acum_cali/acum_credi
 
-def function_prestamo(pagos, credi_r):
-    if(pagos=="si" and credi_r<9):
+def function_carga(unidades):
+    carg=0
+    pos=0
+    for credi in unidades:
+        carg=carg+unidades[pos][1]
+        pos=pos+1
+    return carg
+
+def function_reprobadas(unidades):
+    reprob=0
+    pos=0
+    for cali in unidades:
+        if(unidades[pos][0]<70):
+            reprob=reprob+1
+        pos=pos+1
+    return reprob
+
+def function_creditos(unidades):
+    credi_r=0
+    pos=0
+    for credi in unidades:
+        if(unidades[pos][0]<70):
+            credi_r=credi_r+unidades[pos][1]
+        pos=pos+1
+    return credi_r
+
+def function_prestamo(pagos, unidades):
+    if(pagos=="si" and (function_creditos(unidades))<9):
         print("Conservas tu préstamo educativo")
     else:
         print("Se te retirara tu préstamo educativo")
 
-def function_beca(cali_lista, credi_lista, carg, beca, aprob):
-    if((function_promedio(cali_lista, credi_lista))>=85 and carg>=18 and beca==70 and aprob==0):
+def function_beca(unidades, beca):
+    if((function_promedio(unidades))>=85 and (function_carga(unidades))>=18 and beca==70 and (function_reprobadas(unidades))==0):
         print("Conservas tu beca")
     else:
         print("Se te retirara la beca")
         
-def function_creditos(credi_r):
-    if (credi_r>=9):
+def function_estatus(unidades):
+    if ((function_creditos(unidades))>=9):
         print("Tu estatus academico es Condicional")
     else:
         print("Tu estatus academico es Regular")
-
-aprob=0
-carg=0
-credi_r=0
-
-cali_lista=[]
-credi_lista=[]
-suma=0
-cont=1
+        
+def function_imprimir(unidades):
+    print(" ________________________________________________")
+    print('{:^10}{:^10}{:^10}{:^10}'.format('| Unidad |',' Calificación ','| Créditos |',' Ponderación |'))
+    acum_cali=0.0
+    acum_credi=0
+    pos=0
+    cont=1
+    for cali in unidades:
+        acum_cali=acum_cali+(unidades[pos][0]*unidades[pos][1])
+        acum_credi=acum_credi+unidades[pos][1]
+        print('|{:^8}|{:^14}|{:^10}|{:^13}|'.format(cont,unidades[pos][0],unidades[pos][1],unidades[pos][0]*unidades[pos][1]))
+        pos=pos+1
+        cont=cont+1
+    print(" ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯")
 
 
 uni=int(input("¿Cuántas unidades llevaste al semestre? "))
 
-for a in range(uni):
+cont=1
+i=0
+unidades=[]
+for cali in range(uni):
+    unidad=[]
     cali=float(input("Calificación de la unidad {} sobre 100: ".format(cont)))
+    unidad.append(cali)
     credi=int(input("Cantidad de creditos que equivale la unidad {}: ".format(cont)))
-    if (cali<70):
-        aprob=aprob+1
-        credi_r=credi_r+credi
-    carg=carg+credi
-    cali_lista.append(cali)
-    credi_lista.append(credi)
+    unidad.append(credi)
+    unidades.append(unidad)
+    i==i+1
     cont=cont+1
-
+    
+    
 beca=int(input("¿Cuántas horas de servicio de becario hiciste? "))
 pagos=str(input("¿Estas al corriente de tus pagos? Escribe 'si' o 'no' "))
 
-print("\nTu promedio general es: ", "%.2f"% function_promedio(cali_lista, credi_lista))
+function_imprimir(unidades)
 
-if(aprob>=1):
+print("Tu promedio general es: ", "%.2f"% function_promedio(unidades))
+
+if((function_reprobadas(unidades))>=1):
     print("Reprobaste ",aprob," materias")
 else:
     print("Aprobaste todas tus materias")
 
-function_prestamo(pagos, credi_r)
+function_prestamo(pagos, unidades)
 
-function_creditos(credi_r)
+function_estatus(unidades)
 
-function_beca(cali_lista, credi_lista, carg, beca, aprob)
+function_beca(unidades, beca)
 
 mostrar=str(input("¿Quieres ver como se calcula el promedio? "))
 
